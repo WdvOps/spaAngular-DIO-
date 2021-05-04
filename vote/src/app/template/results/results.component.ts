b134cc25043ece6c30693483307eba79e387e84a
wdvOps <dvops@outlook.com>
2021-05-03 22:44:50 -0300

    Added results

--- vote/src/app/template/results/results.component.ts
+++ vote/src/app/template/results/results.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
+  resultadoJedi = 2;
+  resultadoEmpire = 1;
+
+
   constructor() { }
 
   ngOnInit(): void {
